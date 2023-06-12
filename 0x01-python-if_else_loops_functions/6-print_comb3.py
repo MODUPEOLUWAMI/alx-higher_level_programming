1dcbea229db1e9c3cc2b82238a275833fe097cba
MODUPEOLUWAMI <modutoria@gmail.com>
2023-06-12 12:32:42 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,5 +5,5 @@ for x in range(0, 9):
             continue
         elif x == 8 and y == 9:
             print("{}{}".format(x, y))
-                    else:
-                        print("{}{}".format(x, y), end=", ")
+             else:
+                  print("{}{}".format(x, y), end=", ")
